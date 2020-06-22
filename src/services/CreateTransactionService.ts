771a34f2f380fe6c726062c252446c502cc0f9e2
Dtrusman <dtrusman@gmail.com>
2020-06-22 19:07:10 -0300

    First commit

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,14 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+type TransactionType = 'income' | 'outcome';
+
+interface Request {
+  title: string;
+  value: number;
+  type: TransactionType;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +16,32 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('Type field accept only income or outcome');
+    }
+
+    if (!value) {
+      throw Error('Value is mandatory');
+    }
+
+    if (!title) {
+      throw Error('Title is mandatory');
+    }
+
+    if (type === 'outcome' && value > total) {
+      throw Error('Insufficient funds');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
